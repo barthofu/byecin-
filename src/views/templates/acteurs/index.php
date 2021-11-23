baa6f7b28ac90098eee991f201898e420522d5d1
Bartholomé Gili <bartholome.gili@etu.univ-lyon1.fr>
2021-11-24 00:09:31 +0100

    finished actors page

--- src/views/templates/acteurs/index.php
+++ src/views/templates/acteurs/index.php
@@ -1,13 +1,24 @@
-<div>
-    <ul>
-        <?php
+<div class="acteurs">
 
-        var_dump($data['acteurs']);
+    <div class="acteursContainer">
 
-        ?>
-    </ul>
+        <?php foreach ($data['acteurs'] as $key => $acteur) { ?>
 
-    <a href="<?= getURI('/acteurs/add') ?>" class="add">
-        Ajouter
-    </a>
+            <a class="acteur" href="<?= getURI('/acteurs/get?id='.$acteur->getId()) ?>">
+
+                <div class="nom"><?= $acteur->getNom() . ' ' . $acteur->getPrenom() ?></div>
+                <div class="nbOfFilms">Nombre de films : <span><?= count($acteur->getFilms()) ?></span></div>
+            </a>
+
+        <?php } ?>
+
+    </div>
+
+    <?php if (isLoggedIn()) { ?>
+
+        <a href="<?= getURI('/acteurs/add') ?>" class="add">
+            Ajouter
+        </a>
+
+    <?php } ?>
 </div>
